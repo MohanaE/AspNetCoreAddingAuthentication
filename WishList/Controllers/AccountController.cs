c783a17792e7d4443c8cb49488e05fda70dcc6b1
MohanaE <86883672+MohanaE@users.noreply.github.com>
2021-07-10 09:56:06 +0000

    task4

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using WishList.Models.AccountViewModels;
 using Microsoft.AspNetCore.Authorization;
-using WishList.Models;
 using Microsoft.AspNetCore.Identity;
-/*using WishList.Models.AccountViewModels;*/
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
 
 namespace WishList.Controllers
 {
@@ -44,5 +45,28 @@ namespace WishList.Controllers
             //var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
             return RedirectToAction("HomeController.Index");
         }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            return RedirectToAction("Index", "Item");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
+
